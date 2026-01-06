a460818103425667947a38f32af9ef10b8a27222
pa8lit0 <kapela200@gmail.com>
2026-01-06 15:48:28 +0100

    Add files via upload

---
+++ praca_ze_zmiennymi.py
@@ -0,0 +1,14 @@
+# Najpierw utwórz zmienną o nazwie x i przypisz jej wartość 10
+x = 10
+
+# Następnie utwórz drugą zmienną y i przypisz jej wartość 55
+y = 55
+
+# Utwórz zmienną z i przypisz jej wartość x + y
+z = x + y
+
+# Zastąp x wartością z
+x = z
+
+# Wydrukuj wartość x
+print(x)
